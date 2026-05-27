d84a5721d2a63eb6a615c6159a3eb2ffa5915f4e
ajz34 <ajz34@outlook.com>
2026-05-27 15:04:43 +0800

    time profile update

--- src/numint_matmul/pure_xcpot.rs
+++ src/numint_matmul/pure_xcpot.rs
@@ -13,6 +13,7 @@ use NIDenType::*;
 ///
 /// Note this function does not use the scratch buffer `buf`, so probably there has some allocation
 /// cost, but the code is very simple.
+#[allow(unused)]
 fn contract_ao_wv_without_buf(
     den_type: NIDenType,
     wv: TsrView,
@@ -72,6 +73,82 @@ fn contract_ao_wv_without_buf(
     Ok(())
 }
 
+fn contract_ao_wv(
+    den_type: NIDenType,
+    wv: TsrView,
+    ao: TsrView,
+    mut out: TsrMut,
+    buf: &mut [f64],
+) -> Result<(), NIError> {
+    ni_check_shape!(wv.ndim(), 2, "Weight vector must be 2-dim")?;
+    let nvar = wv.shape()[1];
+    let ngrids = wv.shape()[0];
+    ni_check_shape!(den_type.num_nvar(), nvar, "Dimension mismatch for input density type")?;
+    ni_check_shape!(ao.ndim(), 3, "AO tensor must be 3-dim")?;
+    let nao = ao.shape()[1];
+    ni_check_shape!(ao.shape()[0], ngrids, "AO grids dimension mismatch")?;
+    ni_check_shape!(ao.shape()[2] >= den_type.num_ao_comp(), "AO component dimension insufficient")?;
+
+    ni_check_shape!(out.shape(), [nao, nao], "Output shape mismatch")?;
+    ni_check_shape!(buf.len() >= ngrids * nao, "Buffer length insufficient")?;
+
+    if den_type == LAPL {
+        return Err(ni_error!("Contracting AO with LAPL density type is not supported"));
+    }
+
+    // clean notation of slice, just for readability
+
+    /// Macro for slicing AO tensor with the last index.
+    /// Returns [ngrids, nao] view for the specified component index.
+    macro_rules! ao_ {
+        [$idx:expr] => {
+            ao.i((.., .., $idx))
+        };
+    }
+    /// Macro for slicing weight vector with the last index.
+    /// Returns [ngrids] view for the specified component index.
+    macro_rules! wv_ {
+        [$idx:expr] => {
+            wv.i((.., $idx))
+        };
+    }
+
+    let device = out.device().clone();
+    let mut scr = rt::asarray((buf, [ngrids, nao], &device));
+
+    // RHO contribution
+    // out += 0.5 * ao_![0].t() % (wv_![0] * ao_![0]);
+    rt::mul_with_output(ao_![0], wv_![0], scr.view_mut());
+    out.matmul_from(ao_![0].t(), scr.view(), 0.5, 0.0);
+    // SIGMA contribution
+    if matches!(den_type, SIGMA | TAU) {
+        // out += ao_![1].t() % (wv_![1] * ao_![0]);
+        rt::mul_with_output(ao_![0], wv_![1], scr.view_mut());
+        out.matmul_from(ao_![1].t(), scr.view(), 1.0, 1.0);
+        // out += ao_![2].t() % (wv_![2] * ao_![0]);
+        rt::mul_with_output(ao_![0], wv_![2], scr.view_mut());
+        out.matmul_from(ao_![2].t(), scr.view(), 1.0, 1.0);
+        // out += ao_![3].t() % (wv_![3] * ao_![0]);
+        rt::mul_with_output(ao_![0], wv_![3], scr.view_mut());
+        out.matmul_from(ao_![3].t(), scr.view(), 1.0, 1.0);
+    }
+    // TAU contribution
+    if matches!(den_type, TAU) {
+        // out += 0.25 * ao_![1].t() % (wv_![4] * ao_![1]);
+        rt::mul_with_output(ao_![1], wv_![4], scr.view_mut());
+        out.matmul_from(ao_![1].t(), scr.view(), 0.25, 1.0);
+        // out += 0.25 * ao_![2].t() % (wv_![4] * ao_![2]);
+        rt::mul_with_output(ao_![2], wv_![4], scr.view_mut());
+        out.matmul_from(ao_![2].t(), scr.view(), 0.25, 1.0);
+        // out += 0.25 * ao_![3].t() % (wv_![4] * ao_![3]);
+        rt::mul_with_output(ao_![3], wv_![4], scr.view_mut());
+        out.matmul_from(ao_![3].t(), scr.view(), 0.25, 1.0);
+    }
+    let out_t = out.t().to_owned();
+    out += &out_t;
+    Ok(())
+}
+
 /// Evaluate XC potential (1st order) with vxc_eff.
 ///
 /// # Parameters
@@ -107,7 +184,7 @@ pub fn rks_vxc_pot_with_output(
     }
 
     let vxc_contracted = weights * vxc_eff;
-    contract_ao_wv_without_buf(den_type, vxc_contracted.view(), ao, vxc, buf)
+    contract_ao_wv(den_type, vxc_contracted.view(), ao, vxc, buf)
 }
 
 pub fn rks_fxc_pot_with_output(
@@ -140,7 +217,7 @@ pub fn rks_fxc_pot_with_output(
     let fxc_eff_weighted = &weights * &fxc_eff;
     for i in 0..nset {
         let fxc_contracted = (&fxc_eff_weighted * rho1.i((.., .., None, i))).sum_axes(1);
-        contract_ao_wv_without_buf(den_type, fxc_contracted.view(), ao.view(), fxc.i_mut((.., .., i)), buf)?;
+        contract_ao_wv(den_type, fxc_contracted.view(), ao.view(), fxc.i_mut((.., .., i)), buf)?;
     }
     Ok(())
 }
@@ -190,6 +267,7 @@ pub fn rks_kxc_pot_with_output(
     }
 
     let kxc_eff_weighted = &weights * &kxc_eff;
+
     for i2 in 0..nset2 {
         for i1 in 0..nset1 {
             // Two-step contraction: first contract kxc_eff with rho1, then with rho2
@@ -197,9 +275,26 @@ pub fn rks_kxc_pot_with_output(
             let temp = (&kxc_eff_weighted * &rho1_slice).sum_axes(1); // [ngrids, nvar, nvar]
             let rho2_slice = rho2.i((.., .., None, i2)); // [ngrids, nvar, 1]
             let kxc_contracted = (&temp * &rho2_slice).sum_axes(1); // [ngrids, nvar]
-            contract_ao_wv_without_buf(den_type, kxc_contracted.view(), ao.view(), kxc.i_mut((.., .., i1, i2)), buf)?;
+            contract_ao_wv(den_type, kxc_contracted.view(), ao.view(), kxc.i_mut((.., .., i1, i2)), buf)?;
         }
     }
+
+    /*
+    (0..nset2 * nset1).into_par_iter().for_each(|i12| {
+        let mut buf = buf.to_vec();
+        let i1 = i12 % nset1;
+        let i2 = i12 / nset1;
+        // Two-step contraction: first contract kxc_eff with rho1, then with rho2
+        let rho1_slice = rho1.i((.., .., None, None, i1)); // [ngrids, nvar, 1, 1]
+        let temp = (&kxc_eff_weighted * &rho1_slice).sum_axes(1); // [ngrids, nvar, nvar]
+        let rho2_slice = rho2.i((.., .., None, i2)); // [ngrids, nvar, 1]
+        let kxc_contracted = (&temp * &rho2_slice).sum_axes(1); // [ngrids, nvar]
+        let kxc_view = kxc.i((.., .., i1, i2));
+        let kxc_view = unsafe { kxc_view.force_mut() };
+        contract_ao_wv(den_type, kxc_contracted.view(), ao.view(), kxc_view, &mut buf).unwrap();
+    });
+    */
+
     Ok(())
 }
 
@@ -240,7 +335,7 @@ pub fn uks_vxc_pot_with_output(
 
     for s in 0..2 {
         let vxc_contracted = &weights * vxc_eff.i((.., .., s));
-        contract_ao_wv_without_buf(den_type, vxc_contracted.view(), ao.view(), vxc.i_mut((.., .., s)), buf)?;
+        contract_ao_wv(den_type, vxc_contracted.view(), ao.view(), vxc.i_mut((.., .., s)), buf)?;
     }
     Ok(())
 }
@@ -292,7 +387,7 @@ pub fn uks_fxc_pot_with_output(
             // Contract over the inner spin+var pair (axes 1 and 2)
             let fxc_contracted =
                 (&fxc_eff_weighted.i((.., .., .., .., s)) * rho1.i((.., .., .., None, i))).sum_axes([1, 2]);
-            contract_ao_wv_without_buf(den_type, fxc_contracted.view(), ao.view(), fxc.i_mut((.., .., s, i)), buf)?;
+            contract_ao_wv(den_type, fxc_contracted.view(), ao.view(), fxc.i_mut((.., .., s, i)), buf)?;
         }
     }
     Ok(())
@@ -355,13 +450,7 @@ pub fn uks_kxc_pot_with_output(
                 // Step 2: contract temp with rho2 over remaining spin+var pair (axes 1, 2)
                 let rho2_slice = rho2.i((.., .., .., None, i2)); // [ngrids, nvar, 2, 1]
                 let kxc_contracted = (&temp * &rho2_slice).sum_axes([1, 2]); // [ngrids, nvar]
-                contract_ao_wv_without_buf(
-                    den_type,
-                    kxc_contracted.view(),
-                    ao.view(),
-                    kxc.i_mut((.., .., s, i1, i2)),
-                    buf,
-                )?;
+                contract_ao_wv(den_type, kxc_contracted.view(), ao.view(), kxc.i_mut((.., .., s, i1, i2)), buf)?;
             }
         }
     }
