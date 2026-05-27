628e645f6f5f957b7bda9cfa24728b18fcfd1bf2
ajz34 <ajz34@outlook.com>
2026-05-27 21:21:51 +0800

    rename some files to naive

--- src/numint_matmul/mod.rs
+++ src/numint_matmul/mod.rs
@@ -3,9 +3,9 @@
 
 pub mod buffer_pool;
 pub mod nimatmul;
-pub mod pure_eval_rho;
+pub mod pure_eval_rho_naive;
 pub mod pure_eval_rho_parenh;
-pub mod pure_xcpot;
+pub mod pure_xcpot_naive;
 pub mod pure_xcpot_parenh;
 
 #[allow(unused)]
@@ -14,9 +14,9 @@ pub mod prelude {
 
     pub(crate) use super::buffer_pool::*;
     pub(crate) use super::nimatmul::*;
-    pub(crate) use super::pure_eval_rho::*;
+    pub(crate) use super::pure_eval_rho_naive::*;
     pub(crate) use super::pure_eval_rho_parenh::*;
-    pub(crate) use super::pure_xcpot::*;
+    pub(crate) use super::pure_xcpot_naive::*;
     pub(crate) use super::pure_xcpot_parenh::*;
 
     pub(crate) use std::sync::Mutex;
